afac34f52c7583158cff609a53a58814b8a1071e
Tech-A36 <tech-a36@Tech-A36s-MacBook-Air.local>
2019-04-15 22:58:47 +0100

    when correct ans is picked score +1 // when time is up it turns red

--- main.js
+++ main.js
@@ -2,30 +2,53 @@ $(function(event) {
 console.log("DOM is ready");
 
 
-
+//get questions and answers from API
+  function questions() {
   var request = new XMLHttpRequest();
   request.open("GET", "https://opentdb.com/api.php?amount=10&category=9&difficulty=medium&type=multiple");
 
   request.addEventListener("load", function() {
     var data = JSON.parse(this.responseText);
-    console.log(data);
+    // console.log(data);
     $(".q").html(data.results[0].question);
-    $(".a1").html(data.results[0].incorrect_answers[0]);
-    $(".a2").html(data.results[0].correct_answer);
-    $(".a3").html(data.results[0].incorrect_answers[2]);
-    $(".a4").html(data.results[0].incorrect_answers[1]);
+    $("#a1").html(data.results[0].incorrect_answers[0]);
+    $("#a2").html(data.results[0].correct_answer);
+    $("#a3").html(data.results[0].incorrect_answers[2]);
+    $("#a4").html(data.results[0].incorrect_answers[1]);
   })
   request.send();
+}
+questions()
 
 
+// when correct answer is clicked:
+  var answerButton = document.getElementById("a2");
+  answerButton.addEventListener("click", buttonClick);
+  var score = 0;
+  function buttonClick() {
+      console.log("Correct answer!");
+      score++;
+        console.log(score)
+        document.getElementById("userScore").innerHTML = score + " / 10";
+      console.log(score + "/10");
+          questions();
+    }
+
+
+
+// counts down to zero
 var timer = setInterval(function(){
   var time = document.getElementById('secs').innerHTML;
   if(time > 0) {
     time--;
   } else {
     console.log("Ran out of time!");
-    alert("Time's up!");
     clearInterval(timer);
+  }
+  if (time == 0) {
+    var timeUp = document.getElementById('secs');
+    timeUp.style.color = "red";
+    // timeUp.innerHTML = "Time's up!";
   }
   document.getElementById("secs").innerHTML = time;
 }, 1000);
