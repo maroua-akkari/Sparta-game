d10462baa8ead9a2fed9879b97aa9a8b7f944b60
Tech-A36 <tech-a36@Tech-A36s-MacBook-Air.local>
2019-04-17 00:21:09 +0100

    when time runs out, modal shows that lets user play again

--- main.js
+++ main.js
@@ -1,20 +1,28 @@
 $(function(event) {
 console.log("DOM is ready");
 
+
   function quiz() {
     var request = new XMLHttpRequest();
-    request.open("GET", "https://opentdb.com/api.php?amount=10&category=9&difficulty=medium&type=multiple");
+    request.open("GET", "https://opentdb.com/api.php?amount=12&category=22&difficulty=medium&type=multiple");
     request.addEventListener("load", function() {
       var data = JSON.parse(this.responseText);
-      // console.log(data);
-      $(".q").html(data.results[0].question);
+      console.log(data);
+      question(data);
       ansChoices(data);
     });
     request.send();
-  } quiz();
+  }
+  quiz();
 
   var answers = [];
   var correctAns = [];
+  var score = [];
+
+
+  function question(data) {
+    $("#q").html(data.results[0].question);
+  }
 
   function ansChoices(data) {
     correctAns.push(data.results[0].correct_answer+" A");
@@ -23,171 +31,58 @@ console.log("DOM is ready");
     answers.push(data.results[0].incorrect_answers[2]);
     answers.push(correctAns[0]);
 
-    console.log(answers);
     randomAns = answers.sort(function(a,b) {
-      return 0.5 - Math.random();})
+      return 0.5 - Math.random();
+    });
       for (var i = 0; i < randomAns.length; i++) {
         $("#a"+ i).html(randomAns[i]);
       }
-
-      if (true) {
-
-      }
-
-
-      $(".choice").on("click", function(){
-        console.log("clicked this answer");
-      })
-
-      $(".correct").on("click", function(){
-        console.log("correct answer clicked!");
-      })
-
-
-
-      for (var i = 0; i < correctAns.length; i++) {
-        var correct = correctAns[i];
-        $("#a3").addClass("correct");
-      }
-
-    console.log(answers);
-    console.log(correctAns);
   }
 
-      // var answerButton = document.getElementById("a");
-      // answerButton.addEventListener("click", buttonClick);
-      // var score = 0;
-      // function buttonClick() {
-      //     console.log("Correct answer!");
-      //     score++;
-      //       console.log(score)
-      //       document.getElementById("userScore").innerHTML = score + " / 10";
-      //     console.log(score + "/10");
-      //         questions();
-      //   }
+
+
+    $(".choice").on("click", function(){
+      if (this.innerHTML == correctAns[0]) {
+        console.log("Correct answer!!");
+
+        score++;
+        document.getElementById("userScore").innerHTML = score + " / 10";
+
+        console.log(answers);
+        console.log(correctAns);
+
+        answers = [];
+        correctAns = [];
+        console.log(answers);
+        console.log(correctAns);
+        quiz();
+
+
+      } else {
+      console.log("Wrong answer");
+      answers = [];
+      correctAns = [];
+      quiz();
+    }
+
+  });
 
 
 
 // counts down to zero
-    var timer = setInterval(function(){
-      var time = document.getElementById('secs').innerHTML;
-      if(time > 0) {
-        time--;
-      } else {
-        clearInterval(timer);
-      }
-      if (time == 0) {
-        $(".secs").html("Time's up!").css("color", "red");
-      }
-      document.getElementById("secs").innerHTML = time;
-    }, 1000);
-
-
-
-
-
-  // getAns();
-
-  // function ansChoices() {
-  //   getAns();
-  //   randomAns = answers.sort(function(a,b) {
-  //     return 0.5 - Math.random();})
-  //     for (var i = 0; i < randomAns.length; i++) {
-  //       // var ans = randomAns[i];
-  //       $("#a"+ i).html(randomAns[i]);
-  //     }
-  //     console.log(randomAns);
-  // } ansChoices();
-
-
-
-
-//get questions and answers from API and put into arrays
-//   function questions() {
-//     var request = new XMLHttpRequest();
-//     request.open("GET", "https://opentdb.com/api.php?amount=10&category=9&difficulty=medium&type=multiple");
-//     // var answers = [];
-//     request.addEventListener("load", function() {
-//       var data = JSON.parse(this.responseText);
-//       console.log(data);
-// //puts question in html and all answers into an empty array
-//       $(".q").html(data.results[0].question);
-//       // answers.push(data.results[0].incorrect_answers[0]);
-//       // answers.push(data.results[0].incorrect_answers[1]);
-//       // answers.push(data.results[0].incorrect_answers[2]);
-//       // answers.push(data.results[0].correct_answer + " A");
-//     })
-//     request.send();
-//   } questions();
-
-  // function ansChoices() {
-  //   var request = new XMLHttpRequest();
-  //   request.open("GET", "https://opentdb.com/api.php?amount=10&category=9&difficulty=medium&type=multiple");
-  //   var answers = [];
-  //   request.addEventListener("load", function() {
-  //     var data = JSON.parse(this.responseText);
-  //     answers.push(data.results[0].incorrect_answers[0]);
-  //     answers.push(data.results[0].incorrect_answers[1]);
-  //     answers.push(data.results[0].incorrect_answers[2]);
-  //     answers.push(data.results[0].correct_answer + " A");
-  //   })
-  //   request.send();
-  //     console.log(answers);
-  // } ansChoices();
-
-//randomly sorts array - 1st two then second two and so on
-      // randomAns = answers.sort(function(a,b) {
-//         return 0.5 - Math.random();})
-// //put a random answer in each of the html buttons
-//         for (var i = 0; i < randomAns.length; i++) {
-//           var ans = randomAns[i];
-//           $("#a"+ i).html(ans);
-//         }
-//             console.log(randomAns);
-//     });
-//     request.send();
-  //
-  // }
-  // questions()
-
-
-
-
-
-// when correct answer is clicked:
-  // if (answerButton == answers.correct_answer) {
-  //
-  // }
-
-
-  // var answerButton = document.getElementById("a");
-  // answerButton.addEventListener("click", buttonClick);
-  // var score = 0;
-  // function buttonClick() {
-  //     console.log("Correct answer!");
-  //     score++;
-  //       console.log(score)
-  //       document.getElementById("userScore").innerHTML = score + " / 10";
-  //     console.log(score + "/10");
-  //         questions();
-  //   }
-
-
-//
-// // counts down to zero
-//   var timer = setInterval(function(){
-//     var time = document.getElementById('secs').innerHTML;
-//     if(time > 0) {
-//       time--;
-//     } else {
-//       clearInterval(timer);
-//     }
-//     if (time == 0) {
-//       $(".secs").html("Time's up!").css("color", "red");
-//     }
-//     document.getElementById("secs").innerHTML = time;
-//   }, 1000);
-
+  var timer = setInterval(function(){
+    var time = document.getElementById('secs').innerHTML;
+    if(time > 0) {
+      time--;
+    } else {
+      clearInterval(timer);
+    }
+    if (time <= 0) {
+      $(".secs").html("Time's up!").css("color", "red");
+      $('#myModal').modal('show');
+    }
+    document.getElementById("secs").innerHTML = time;
+  }, 1000);
 
 
 
