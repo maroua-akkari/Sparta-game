737fff954252b85ac1a4281a2f8ffcce945290bc
Tech-A36 <tech-a36@Tech-A36s-MacBook-Air.local>
2019-04-16 10:05:34 +0100

    ans now randomly assigned to html button

--- main.js
+++ main.js
@@ -4,54 +4,41 @@ console.log("DOM is ready");
 
 //get questions and answers from API and put into arrays
   function questions() {
-  var request = new XMLHttpRequest();
-  request.open("GET", "https://opentdb.com/api.php?amount=10&category=9&difficulty=medium&type=multiple");
-  // var question = [];
-  var answers = [];
-  request.addEventListener("load", function() {
-    var data = JSON.parse(this.responseText);
-
-    console.log(data);
+    var request = new XMLHttpRequest();
+    request.open("GET", "https://opentdb.com/api.php?amount=10&category=9&difficulty=medium&type=multiple");
+    var answers = [];
+    request.addEventListener("load", function() {
+      var data = JSON.parse(this.responseText);
+//puts question in html and all answers into an empty array
       $(".q").html(data.results[0].question);
-    answers.push(data.results[0].incorrect_answers[0]);
-    answers.push(data.results[0].incorrect_answers[1]);
-    answers.push(data.results[0].incorrect_answers[2]);
-    answers.push(data.results[0].correct_answer);
-
-    // for (var i = 0; i < answers.length; i++) {
-      // var randomAns1 = answers[Math.floor(Math.random()*answers.length)];
-      // var randomAns2 = answers[Math.floor(Math.random()*answers.length)];
-      // var randomAns3 = answers[Math.floor(Math.random()*answers.length)];
-      // var randomAns4 = answers[Math.floor(Math.random()*answers.length)];
+      answers.push(data.results[0].incorrect_answers[0]);
+      answers.push(data.results[0].incorrect_answers[1]);
+      answers.push(data.results[0].incorrect_answers[2]);
+      answers.push(data.results[0].correct_answer);
+//randomly sorts array - 1st two then second two and so on
       randomAns = answers.sort(function(a,b) {
-        return 0.5 - Math.random();
-      })
-      console.log(randomAns);
+        return 0.5 - Math.random();})
 
-      for (var i = 0; i < randomAns.length; i++) {
-        $("#a"+ i).html(randomAns[i]);
-      }
-
-
-  })
-  request.send();
-  console.log(answers);
-}
-questions()
+        for (var i = 0; i < randomAns.length; i++) {
+          $("#a"+ i).html(randomAns[i]);}
+    })
+    request.send();
+  }
+  questions()
 
 
 // when correct answer is clicked:
-  // var answerButton = document.getElementById("a2");
-  // answerButton.addEventListener("click", buttonClick);
-  // var score = 0;
-  // function buttonClick() {
-  //     console.log("Correct answer!");
-  //     score++;
-  //       console.log(score)
-  //       document.getElementById("userScore").innerHTML = score + " / 10";
-  //     console.log(score + "/10");
-  //         questions();
-  //   }
+  var answerButton = document.getElementById("a2");
+  answerButton.addEventListener("click", buttonClick);
+  var score = 0;
+  function buttonClick() {
+      console.log("Correct answer!");
+      score++;
+        console.log(score)
+        document.getElementById("userScore").innerHTML = score + " / 10";
+      console.log(score + "/10");
+          questions();
+    }
 
 
 
